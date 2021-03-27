84524c5fd99f300b91dba583a76498064f3165b8
vtorres96 <victorcdc96@gmail.com>
2021-03-27 12:32:48 -0300

    enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,9 @@
 module.exports = app => {
   app.disable('x-powered-by')
+  app.use((req, res, next) => {
+    res.set('access-controll-allow-origin', '*')
+    res.set('access-controll-allow-methods', '*')
+    res.set('access-controll-allow-headers', '*')
+    next()
+  })
 }
