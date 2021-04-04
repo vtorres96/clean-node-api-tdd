1ffd9446972f99ee2dc233ffbd106042fe245f53
vtorres96 <victorcdc96@gmail.com>
2021-04-04 17:08:19 -0300

    connect to mongodb before listen for app

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,9 @@
-const app = require('./config/app')
+const MongoHelper = require('../infra/helpers/mongo-helper')
+const env = require('./config/env')
 
-app.listen(5858, () => console.log('Server Running'))
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    const app = require('./config/app')
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
